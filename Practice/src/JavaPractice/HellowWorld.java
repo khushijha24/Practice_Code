9568d554f5cb389674725717489028b13d080236
khushijha24 <khushiujan2005@gmail.com>
2024-09-29 13:32:07 +0530

    first commit

---
+++ Practice/src/JavaPractice/HellowWorld.java
@@ -0,0 +1,40 @@
+package JavaPractice;
+import java.util.Scanner;
+
+public class HellowWorld {
+
+	public static void main(String[] args) {
+		 Scanner s = new Scanner(System.in);
+	     int n = s.nextInt();
+
+	     if(n>=0 && n<=50){
+	         int i = 1;
+	          while(i<=n){
+	                int j = 1;
+	                while(j<=n-i){
+	                    System.out.print(" ");
+	                    j= j+1;
+	                }
+	                j = 1;
+	                int p = i;
+	                while(j<=i){
+	                    System.out.print(p);
+	                    j++;
+	                    p++;
+	                }
+
+	                j=1;
+	                p= 2*i-2;
+	                while(j<=i-1){
+	                    System.out.print(p);
+	                    j++;
+	                    p--;
+	                }
+	                System.out.println();
+	                i = i+1;
+	            }
+	        }
+	     s.close();
+	}
+
+}
