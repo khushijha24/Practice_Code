40ceae1fef64c0961b35fb3d72c3b7b55e005b08
khushijha24 <khushiujan2005@gmail.com>
2024-10-06 13:27:10 +0530

    commandsPractice

---
+++ Practice/src/JavaPractice/PrintExample.java
@@ -0,0 +1,18 @@
+package JavaPractice;
+
+public class PrintExample {
+
+	public static void main(String[] args) {
+		String name= "sita";
+		System.out.println(name);
+		
+		
+		DNIUOWONOO
+		
+		
+		DJBNCIOIOOO
+		NCONOWONBOW
+
+	}
+
+}
