baaebc8f4b7ebfa29693f938a217bcbe6a96989d
khushijha24 <khushiujan2005@gmail.com>
2024-10-06 13:31:28 +0530

    updated

--- Practice/src/JavaPractice/PrintExample.java
+++ Practice/src/JavaPractice/PrintExample.java
@@ -5,13 +5,6 @@ public class PrintExample {
 	public static void main(String[] args) {
 		String name= "sita";
 		System.out.println(name);
-		
-		
-		DNIUOWONOO
-		
-		
-		DJBNCIOIOOO
-		NCONOWONBOW
 
 	}
 
